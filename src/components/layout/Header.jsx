c4c02593072ae9ea4f915a2fbe903dc57b665fbc
Nantenaina-Maminirina-Ranaivosoa <rnantenainamaminirina@gmail.com>
2025-12-30 07:49:23 +0300

    feat: add futuristic responsive header with semantic navigation

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,18 @@
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <h1>Digital & Real Estate</h1>
+    <header className="site-header">
+      <nav className="nav-container">
+        <div className="logo">
+          <span>Tech</span>Estate
+        </div>
+
+        <ul className="nav-links">
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Marketing Digital</a></li>
+          <li><a href="#">Immobilier</a></li>
+          <li><a href="#">Compétences</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
       </nav>
     </header>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./styles/variables.css";
 import "./styles/base.css";
+import "./styles/layout.css";
+
 
 
 createRoot(document.getElementById('root')).render(
